5792a078f6bde5c3af22cdc14eef58db0f325075
GrestiBed <siret@sragentuur.ee>
2017-10-23 15:40:05 +0300

    add calculator+number+excuses
    exercises 04, 05 and 08

---
+++ src/01-exercises/01-HelloWorld/05-number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri ([0-100]. Proovi see ära arvata :)");
+
+            int magicNumber = 33;
+
+            Console.Write(Environment.NewLine);
+            Console.Write("Sina pakud: ");
+
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+
+            if (value > magicNumber)
+            {
+                Console.WriteLine("Minu valitud number on väiksem");
+            }
+
+            else
+            {
+                Console.Write("Minu valitud number on suurem");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
