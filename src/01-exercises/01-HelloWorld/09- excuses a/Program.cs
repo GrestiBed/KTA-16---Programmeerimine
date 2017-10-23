5f64eebb7e8a0d93e914593327498ea18244bf31
GrestiBed <siret@sragentuur.ee>
2017-10-23 15:55:49 +0300

    add excuses-1

---
+++ src/01-exercises/01-HelloWorld/09- excuses a/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09__excuses_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutis tiksuda" };
+
+            int i = 0;
+            foreach (var excuse in excuses)
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+                i++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
