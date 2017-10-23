05496ed959f20f7c67e93524dcaa9a382ed90cc9
GrestiBed <siret@sragentuur.ee>
2017-10-23 14:36:45 +0300

    Add Parrot repeat console application

---
+++ src/01-exercises/01-HelloWorld/03-Parrot Repeat Back/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Parrot_Repeat_Back
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myVariable;
+
+            Console.WriteLine("See on papagoi programm. Mina ütlen sulle ja sina kordad mulle selle tagasi..");
+            Console.Write(Environment.NewLine);
+            Console.Write("Sina ütled: ");
+            myVariable = Console.ReadLine();
+            Console.Write("Mina ütlen: ");
+            Console.WriteLine(myVariable);
+            Console.ReadLine();
+        }
+    }
+}
